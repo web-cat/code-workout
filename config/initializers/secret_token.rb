72bdb3a52c6f613544f6e2cc787c498d3d8c647b
s-edwards <edwards@cs.vt.edu>
2013-07-24 23:07:05 -0400

    added bootstrap support

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-CodePractice::Application.config.secret_key_base = '7b9f143f82643c85a497f74d017e9f96148d249aa9491c4bc55fd45418449d42a557b38a18f69befe45526b450a4b397dabb5b58bd52bf75ff76b4b97ca9dca1'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+CodePractice::Application.config.secret_key_base = secure_token
