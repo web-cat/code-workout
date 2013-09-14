814598cb0572ee9b4369709bcccca5a40cb573e0
s-edwards <edwards@cs.vt.edu>
2013-09-13 21:14:32 -0400

    Ran annotate on all models, and generated its .rake file so it will auto-run on new db migrations to keep model annotations up to date.

--- spec/models/course_offering_spec.rb
+++ spec/models/course_offering_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: course_offerings
+#
+#  id                      :integer          not null, primary key
+#  course_id               :integer
+#  term_id                 :integer
+#  name                    :string(255)
+#  label                   :string(255)
+#  url                     :string(255)
+#  self_enrollment_allowed :boolean
+#  created_at              :datetime
+#  updated_at              :datetime
+#
+
 require 'spec_helper'
 
 describe CourseOffering do
--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: organizations
+#
+#  id           :integer          not null, primary key
+#  display_name :string(255)
+#  url_part     :string(255)
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
 require 'spec_helper'
 
 describe Organization do
