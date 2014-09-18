f88537270e6d73b15585d8645ff5d2f0bfba2817
s-edwards <edwards@cs.vt.edu>
2014-09-18 10:07:24 -0400

    Changes to the global role factory definitions.  Temporarily commented out workouts content from sidebar until NPE gets fixed.

--- spec/factories/global_roles.rb
+++ spec/factories/global_roles.rb
@@ -1,24 +1,24 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
-#FactoryGirl.define do
-#  factory :global_role do
-#    name "Administrator"
-#    can_manage_all_courses true
-#    can_edit_system_configuration true
-#    builtin true
-#  end
+FactoryGirl.define do
+  factory :global_role_admin do
+    name "Administrator"
+    can_manage_all_courses true
+    can_edit_system_configuration true
+    builtin true
+  end
 
-#  factory :global_role do
-#  	name "Instructor"
-#  	can_manage_all_courses false
-#  	can_edit_system_configuration false
-#  	builtin false
-#  end
+  factory :global_role_instructor do
+  	name "Instructor"
+  	can_manage_all_courses false
+  	can_edit_system_configuration false
+  	builtin false
+  end
 
-#  factory :global_role do
-#  	name "Regular User"
-#  	can_manage_all_courses false
-#  	can_edit_system_configuration false
-#  	builtin false
-#  end
-#end
+  factory :global_role_user do
+  	name "Regular User"
+ 	  can_manage_all_courses false
+  	can_edit_system_configuration false
+ 	  builtin false
+  end
+end
