83b952a3f858ea271ac5ee3a115d536e511fc176
pmk1433 <metarus208@gmail.com>
2014-11-09 20:14:31 -0500

    Add the 'add_workout' and 'store_workout' methods to CourseOffering controller to process the addition workouts to a course offering.

--- app/controllers/course_offerings_controller.rb
+++ app/controllers/course_offerings_controller.rb
@@ -1,5 +1,5 @@
 class CourseOfferingsController < ApplicationController
-  before_action :set_course_offering, only: [:show, :edit, :update, :destroy]
+  before_action :set_course_offering, only: [:show, :edit, :update, :destroy, :add_workout, :store_workout]
 
   # GET /course_offerings
   def index
@@ -45,6 +45,33 @@ class CourseOfferingsController < ApplicationController
   def destroy
     @course_offering.destroy
     redirect_to course_offerings_url, notice: 'Course offering was successfully destroyed.'
+  end
+  
+  # GET /course_offerings/:id/add_workout
+  def add_workout
+    @workouts=Workout.take(10)
+    @wkts=[]
+    @course_offering.workouts.each do |wks|
+      @wkts<<wks
+    end
+    @workouts=@workouts-@wkts
+    @course_offering    
+  end
+  
+  # POST /course_offerings/store_workout/:id
+  def store_workout
+    workouts=params[:workoutids]
+    workouts.each do |wkid|
+      wk=Workout.find(wkid)
+      @course_offering.workouts<<wk
+      @course_offering.save
+      wek = @course_offering.workout_offerings.find_by_sql("select * from workout_offerings where workout_id=#{wkid} and course_offering_id=#{@course_offering.id}")
+      wek.last.opening_date=params[:opening_date]
+      wek.last.soft_deadline=params[:soft_deadline]
+      wek.last.hard_deadline=params[:hard_deadline]
+      wek.last.save
+    end
+    redirect_to course_offering_path(@course_offering), notice: 'Workouts added to course offering!'    
   end
 
   private
