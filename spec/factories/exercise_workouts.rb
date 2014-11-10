40e68d75a1e22611f1330e87bab715148aeba8fa
pmk1433 <metarus208@gmail.com>
2014-11-09 23:46:13 -0500

    The spec for courses_exercise, course_exercises, exercise_workouts, workout_offerings and exercise_workout has been created.

---
+++ spec/factories/exercise_workouts.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :exercise_workout do
+    exercise_id 1
+    workout_id 1
+    order 1
+    points ""
+  end
+end
