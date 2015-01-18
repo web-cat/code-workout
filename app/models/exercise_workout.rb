5e1d4459a66f4b2a2875a78b014c96e4385c7c95
Krishnan Panamalai Murali <metarus208@gmail.com>
2015-01-17 20:59:16 -0500

    Added a method to return the total points for all exercises within a workout.

--- app/models/exercise_workout.rb
+++ app/models/exercise_workout.rb
@@ -1,4 +1,10 @@
 class ExerciseWorkout < ActiveRecord::Base
   belongs_to :exercise
   belongs_to :workout
+  
+  #return the points for an exercise belonging to a particular workout
+  def self.findExercisePoints(exid,wktid)
+    wex=ExerciseWorkout.find_by(exercise_id: exid, workout_id: wktid)
+    return wex.points
+  end
 end
