2a3ec10e84aea346c6c2b8398a50d7b3e20e0e30
Krishnan Panamalai Murali <metarus208@gmail.com>
2015-01-17 20:56:22 -0500

    Modified the attempt model return an user's score for an exercise/workout.

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -36,9 +36,41 @@ class Attempt < ActiveRecord::Base
   
   belongs_to :exercise
 
-  #TO DO tie attempt to current user session
+  #TODO tie attempt to current user session
   #validates :user, presence: true
   #belongs_to :user
   validates :exercise, presence: true
+  
+  # Returns whether an user has attempted an exercise or not, if attempted returns the latest score
+  def self.user_attempt(uid,ex_id)
+    last_attempt=Attempt.where(user_id: uid, exercise_id: ex_id).where.not(workout_offering_id: nil).last
+    if last_attempt.nil?
+      return false
+    else
+      return last_attempt.score
+    end
+  end
+  
+  # Returns the user's score for a particular worked by combing through attempts
+  def self.getUserScore(uid,wktid)
+    wkt_len=Workout.find(wktid).exercises.length;
+    relevant_workout_offerings=WorkoutOffering.where(workout_id: wktid).to_a
+    workouts_offering_ids=Array.new
+    relevant_workout_offerings.each do |wkt|
+      workouts_offering_ids<<wkt.id
+    end
+    user_attempts=Attempt.where(user_id: uid,workout_offering_id: workouts_offering_ids);
+    user_attempts_array=user_attempts.to_a
+    user_end = user_attempts_array.last.id;
+    puts "GOSHIEN user end"
+    puts user_end
+    puts "Ghoshien"
+    user_wkt_attempts=Attempt.where("id<=? and id>?",user_end,user_end-wkt_len ).to_a;
+    final_score=0
+    user_wkt_attempts.each do |attempt|
+      final_score+=attempt.score
+    end
+    return final_score
+  end
   
 end
