3c18b84d4302159800b47eedc824c28e7fcaa44b
Krishnan Panamalai Murali <metarus208@gmail.com>
2015-02-14 23:47:24 -0500

    Modified the ExerciseHelper to use the new Java coding problem logic.

--- app/helpers/exercises_helper.rb
+++ app/helpers/exercises_helper.rb
@@ -6,27 +6,33 @@ module ExercisesHelper
       i+=1
       if language=='Ruby'
         tests=tests+"\n\tdef test#{base_class}#{i}"+"\n"
-        tests=tests+"\t\tif #{test_method}(#{test_case.input})==#{test_case.expected_output}"+"\n"
+        tests=tests+"\t\tif #{test_method}(#{test_case.input.gsub(";",",")})==#{test_case.expected_output}"+"\n"
         tests=tests+"\t\t\t@@f.write(\"1,,#{i}\\n\") "+"\n"+"\t\telse\n"
         tests=tests+"\t\t\t@@f.write(\"0,#{test_case.negative_feedback.chomp},#{i}\\n\")"+"\n"
         tests=tests+"\t\tend"+"\n"
-        tests=tests+"\t\tassert_equal(#{test_method}(#{test_case.input}),#{test_case.expected_output})"+"\n"
-        tests=tests+"\tend\n "
+        tests=tests+"\t\tassert_equal(#{test_method}(#{test_case.input.gsub(";",",")}),#{test_case.expected_output})"+"\n"
+        tests=tests+"\tend\n"
       
       elsif language=='Python'
         tests=tests+"\n\tdef test#{i}(self):"+"\n"
-        tests=tests+"\t\tif #{base_class}.#{test_method}(#{test_case.input})==#{test_case.expected_output}:"+"\n"
+        tests=tests+"\t\tif #{base_class}.#{test_method}(#{test_case.input.gsub(";",",")})==#{test_case.expected_output}:"+"\n"
         tests=tests+"\t\t\t#{base_class}Test.f.write(\"1,,#{i}\\n\")"+"\n"+"\t\telse:\n"
         tests=tests+"\t\t\t#{base_class}Test.f.write(\"0,"+test_case.negative_feedback.chomp+','+i.to_s+'\n")'+"\n"
-        tests=tests+"\t\tself.assertEqual(#{base_class}.#{test_method}(#{test_case.input}),#{test_case.expected_output})"+"\n "
+        tests=tests+"\t\tself.assertEqual(#{base_class}.#{test_method}(#{test_case.input.gsub(";",",")}),#{test_case.expected_output})"+"\n"
       
       elsif language=='Java'
-        tests=tests+"{#{test_case.input},#{test_case.expected_output}},"   
+        tests=tests+"\n\t@Test"
+        tests=tests+"\n\tpublic void test#{test_method}#{i}() {"
+        tests=tests+"\n\t\tif ( f.#{test_method}(#{test_case.input.gsub(";",",")}) == #{test_case.expected_output} )"
+        tests=tests+"\n\t\t\tfout.println(\"1,,\"+cnt);"
+        tests=tests+"\n\t\telse"
+        tests=tests+"\n\t\t\tfout.println(\"0,#{test_case.negative_feedback.chomp},\"+cnt);"
+        tests=tests+"\n\t\tassertEquals( f.#{test_method}(#{test_case.input.gsub(";",",")}),#{test_case.expected_output});\n}\n"   
       
       end
       
     end
-    return tests.chop
+    return tests
   end
   
   
