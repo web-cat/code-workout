238b3f3879a3b79ab3f985bf5da621a5a65a7ce4
s-edwards <edwards@cs.vt.edu>
2015-02-16 10:34:16 -0500

    Enabled codemirror support for coding question practice page.  Minor layout tweaks to correct problems with page width.

--- app/assets/javascripts/cm.js
+++ app/assets/javascripts/cm.js
@@ -1,3 +1,4 @@
+var codemirror;
 $(function() {
   $("pre").each(function() {
     var codeNode = this;
@@ -12,5 +13,24 @@ $(function() {
       readOnly: true,
       lineNumbers: $(codeNode).hasClass('lineNumbers') || !$(codeNode).hasClass('noLineNumbers')
     });
+  });
+
+  $(".practice textarea").each(function() {
+  	var codeNode = this;
+  	codemirror = CodeMirror.fromTextArea(codeNode, {
+      theme: 'aptana',
+      mode: $(codeNode).data('lang') || 'text/x-java',
+      indentUnit: 4,
+      autoClearEmptyLines: true,
+      fixedGutter: true,
+      matchBrackets: true,
+      /* autofocus: true, */
+      lineNumbers: true,
+      viewportMargin: 150
+  	});
+  	/* For some reason, CodeMirror's focus() doesn't seem to work
+  	 * correctly, so need to leave it turned off until we can fix it.
+  	 * Once fixed, remove the comment for autofocus above.
+  	 */
   });
 });
