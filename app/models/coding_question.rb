8c8b67ea1c57abfd8cc31ec64e56698826f4b4f7
s-edwards <edwards@cs.vt.edu>
2015-02-16 20:47:22 -0500

    Minor code refactoring in exercise. Formatting changes.

--- app/models/coding_question.rb
+++ app/models/coding_question.rb
@@ -1,14 +1,14 @@
 class CodingQuestion < ActiveRecord::Base
-  
-  
+
+
   #~ Validations
-  validates :wrapper_code, presence: true, length: {minimum: 1}
-  validates :test_script, presence: true, length: {minimum: 1}
-  
+  validates :wrapper_code, presence: true, length: { minimum: 1 }
+  validates :test_script, presence: true, length: { minimum: 1 }
+
   #~ Relationships
   belongs_to :exercise
   has_many :test_cases
-  
+
   #~ Acceptance
-  accepts_nested_attributes_for :test_cases, :allow_destroy => true
+  accepts_nested_attributes_for :test_cases, allow_destroy: true
 end
