a920e5d7d79dd6c9de835c2f0427cdd125d95cdf
s-edwards <edwards@cs.vt.edu>
2015-03-18 14:01:12 -0400

    Excluding bootstrap-social from the asset precompilation sequence, since it isn't separately used anywhere.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,6 +1,6 @@
 # config/initializers/assets.rb
 Rails.application.config.assets.precompile << Proc.new do |path|
-  if path =~ /\.(css|js)\z/
+  if path =~ /\.(css|js)\z/ && path !~ /bootstrap-social/
     full_path = Rails.application.assets.resolve(path).to_path
     app_assets_path = Rails.root.join('app', 'assets').to_path
     if full_path.starts_with? app_assets_path
