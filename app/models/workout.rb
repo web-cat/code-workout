6512d523a401b8021645c95289d5e48c3f9f384a
Krishnan Panamalai Murali <metarus208@gmail.com>
2015-05-04 06:31:05 -0400

    Associating workouts with attempts.

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -22,6 +22,7 @@ class Workout < ActiveRecord::Base
 	  inverse_of: :workout, dependent: :destroy
 	has_many :workout_scores, inverse_of: :workout, dependent: :destroy
   has_many :users, through: :workout_scores
+  has_many :attempts
 	has_and_belongs_to_many :tags
   has_many :workout_offerings, inverse_of: :workout, dependent: :destroy
   has_many :course_offerings, through:  :workout_offerings
