4ef9664ee8593cc5d18a1293d1af2d0cb3c53e47
krishnan <metarus208@gmail.com>
2015-09-13 15:40:00 -0400

    Factory spec for time zones.

---
+++ spec/factories/time_zones.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :time_zone do
+    name "MyString"
+    zone "MyString"
+    display_as "MyString"
+  end
+end
