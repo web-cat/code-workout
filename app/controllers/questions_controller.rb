56d68de447e34a09c62f74aa1200b8de38f6fa3a
Jacob Stoebel <stoebelj@berea.edu>
2016-03-12 13:24:56 -0500

    actions for new, create, edit and update added controller actions to create and update a question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,15 +6,46 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    #pre: nothing
+    #post: a new question object
+      #question#new is rendered
+
+    @question = Question.new
+
+    #TODO we need a route for users to ask a question about a paticular exercise.
+
+
   end
 
   def create
+    @question = Question.new(safe_assign)
+    @question.user_id = current_user.id
+
+    if @question.save
+      flash[:success] = "Question saved!"
+      redirect_to questions_path
+    else
+      flash[:error] = "Error creating your question."
+      render 'new'
+    end
+
   end
 
   def edit
+    @question = Question.find(params[:id])
   end
 
   def update
+    @question = Question.find(params[:id])
+    @question.assign_attributes(safe_assign)
+
+    if @question.save
+      flash[:success] = "Question saved!"
+      redirect_to questions_path
+    else
+      flash[:error] = "Error updating question."
+      render 'new'      
+    end
   end
 
   def delete
@@ -22,4 +53,10 @@ class QuestionsController < ApplicationController
 
   def destroy
   end
+
+  private
+  def safe_assign
+    params.require(:question).permit(:title, :body, :tags)
+  end
+
 end
