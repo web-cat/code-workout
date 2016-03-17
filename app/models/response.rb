5805f8b5cc7b308cb1d63de424f082046eeb9ff1
aumcgl <ausmcg335ej@gmail.com>
2016-03-17 16:14:18 -0400

    Added a validation for response content The response model has a new validation that checks if the user has entered any content for the text field of a response. This is meant to protect against Nil values in the database.

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,12 @@
 class Response < ActiveRecord::Base
+
+    # ASSOCIATIONS
+
     belongs_to :question
     belongs_to :user
+
+    # VALIDATIONS
+
+    validates :text, presence: {message: "A response has not been entered."}
+
 end
