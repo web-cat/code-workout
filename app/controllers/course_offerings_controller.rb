d4f29467ae9297f39077fc7965f093c1c8fd499f
Krishnan Panamalai Murali <metarus208@gmail.com>
2016-03-22 21:33:15 -0400

    Ordering the gradebook by last name, first name and email respectively.

--- app/controllers/course_offerings_controller.rb
+++ app/controllers/course_offerings_controller.rb
@@ -145,6 +145,8 @@ class CourseOfferingsController < ApplicationController
 
   # -------------------------------------------------------------
   def generate_gradebook
+    @course_enrolled = CourseEnrollment.where(course_offering: @course_offering).
+                         sort_by{|ce| [ce.user.last_name.downcase, ce.user.first_name.downcase, ce.user.email]}
     respond_to do |format|
       format.csv do
         headers['Content-Disposition'] =
