d772eac3831969adc9842004499edae3bad8aef3
Ayaan Kazerouni <ayaan@vt.edu>
2016-06-06 13:41:53 -0400

    Added admin interface for workout_offerings.

---
+++ app/admin/workout_offering.rb
@@ -0,0 +1,67 @@
+ActiveAdmin.register WorkoutOffering do
+  includes :workout, :course_offering
+  active_admin_import
+
+  menu parent: 'University-oriented', priority: 40
+  permit_params :course_offering_id, :workout_id, :opening_date, :soft_deadline, :hard_deadline
+
+  index do
+    id_column
+    column :course_offering_id, sortable: 'course_offering.label'
+    column :workout_id
+    column :opening_date
+    column :soft_deadline
+    column :hard_deadline
+    column :created_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :course_offering_id, sortable: 'course_offering.label'
+      row :workout_id
+      row :opening_date
+      row :soft_deadline
+      row :hard_deadline
+      row :created_at
+      row :updated_at
+    end
+
+    # panel 'Roster' do
+    #   table_for course_offering.students do
+    #     column :name, :display_name
+    #     column :email
+    #   end
+    # end
+
+  end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :course_offering
+      f.input :workout
+      f.input :opening_date
+      f.input :soft_deadline
+      f.input :hard_deadline
+    end
+    f.actions
+  end
+
+  # sidebar 'Instructors', only: :show,
+  #   if: proc{ course_offering.instructors.any? } do
+  #   table_for course_offering.instructors do
+  #     column(:name) { |i| link_to i.display_name, admin_user_path(i) }
+  #     column(:email) { |i| link_to i.email, 'mailto:' + i.email }
+  #   end
+  # end
+  #
+  # sidebar 'Graders', only: :show,
+  #   if: proc{ course_offering.graders.any? } do
+  #   table_for course_offering.graders do
+  #     column(:name) { |i| link_to i.display_name, admin_user_path(i) }
+  #     column(:email) { |i| link_to i.email, 'mailto:' + i.email }
+  #   end
+  # end
+
+end
