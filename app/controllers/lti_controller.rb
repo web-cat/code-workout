a9ae84b628858dfea189db32f014425f5a4e2134
Hosam Shahin <hosamlshahin@gmail.com>
2016-08-06 17:32:09 +0200

    Minor change to regex.

--- app/controllers/lti_controller.rb
+++ app/controllers/lti_controller.rb
@@ -116,10 +116,10 @@ class LtiController < ApplicationController
 
         workout_name = params[:resource_link_title]
 
-        if (/\A[0-9][0-9]-[0-9][0-9]-[0-9][0-9]/ =~ workout_name).nil?
+        if (/\A[0-9][0-9].[0-9][0-9].[0-9][0-9] -/ =~ workout_name).nil?
           @workout = Workout.find_by(name: workout_name)
         else
-          @workout = Workout.find_by(name: workout_name[9..workout_name.length])
+          @workout = Workout.find_by(name: workout_name[11..workout_name.length])
         end
 
         if @workout.blank?
