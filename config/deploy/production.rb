f34328cd6700387296d05e412f440177ab931ca5
s-edwards <edwards@cs.vt.edu>
2016-08-17 21:35:01 -0400

    Configured for staging server/env deployment.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,7 @@ role :db,  %w{codeworkout@codeworkout.org}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'codeworkout.org', user: 'codeworkout', roles: %w{web app db}, my_property: :my_value
+server 'codeworkout.cs.vt.edu', user: 'codeworkout', roles: %w{web app db}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
