e338c83beea4af0a770c6767753eaff806cc39f1
Ayaan Kazerouni <ayaan@vt.edu>
2018-01-12 13:05:03 -0500

    Runs on ruby >= 2.3. Fixed asset precompilation

--- config/environment.rb
+++ config/environment.rb
@@ -2,10 +2,6 @@
 require File.expand_path('../application', __FILE__)
 require 'rails_extensions'
 
-# Generate non-pretty-printed HTML even in development (it's faster, and
-# with tools like Firebug and the Webkit inspector, we don't need to look
-# directly at the source)
-Haml::Template.options[:ugly] = true
 
 # Initialize the Rails application.
 CodeWorkout::Application.initialize!
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,18 +1,18 @@
 # config/initializers/assets.rb
-# Rails.application.config.assets.precompile << Proc.new do |path|
-#   if path =~ /\.(css|js)\z/ && path !~ /bootstrap-social/
-#     full_path = Rails.application.assets.resolve(path).to_path
-#     app_assets_path = Rails.root.join('app', 'assets').to_path
-#     if full_path.starts_with? app_assets_path
-#       # logger.info "including asset: " + full_path
-#       true
-#     else
-#       # logger.info "excluding asset: " + full_path
-#       false
-#     end
-#   else
-#     false
-#   end
-# end
+ Rails.application.config.assets.precompile << Proc.new do |path|
+   if path =~ /\.(css|js)\z/ && path !~ /bootstrap-social/
+     full_path = Rails.application.assets.resolve(path)
+     app_assets_path = Rails.root.join('app', 'assets').to_path
+     if full_path.starts_with? app_assets_path
+       # logger.info "including asset: " + full_path
+       true
+     else
+       # logger.info "excluding asset: " + full_path
+       false
+     end
+   else
+     false
+   end
+ end
 
-Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.mustache.html *.js *.css)
+#Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.mustache.html *.js *.css)
