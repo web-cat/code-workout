67bde43a049d3df000e51444f7fdd0beea6c4e27
Ayaan Kazerouni <ayaan@vt.edu>
2018-12-03 11:54:29 -0500

    Fix workout clone permission.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -271,7 +271,7 @@ class Ability
     # and use the Workout VIEWS, not controller actions.
     # So this permission affects ONLY gym access.
     can [:read, :practice, :embed], Workout, is_public: true
-    can :clone, Workout, is_public: true
+    can :clone, Workout
   end
 
   def process_workout_offerings(user)
