9a5b61879c54f25607d528b77bb6801f0391cf92
Ayaan Kazerouni <ayaan@vt.edu>
2019-01-22 15:29:39 -0500

    Fix deploy syntax

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -17,7 +17,7 @@ role :db,  %w{codeworkout@codeworkoutdev.cs.vt.edu}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'codeworkoutdev.cs.vt.edu', user: 'codeworkout', roles: %w{web app db}, branch: 'staging', my_property: :my_value
+server 'codeworkoutdev.cs.vt.edu', user: 'codeworkout', roles: %w{web app db}, branch: 'rails-5-upgrade', my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
