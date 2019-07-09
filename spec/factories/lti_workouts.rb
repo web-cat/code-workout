6e5d28227c75b6fe0f5883ac4be75dae6672178d
Ayaan Kazerouni <ayaan@vt.edu>
2019-07-09 13:39:43 -0400

    Update FactoryGirl to FactoryBot
    For LTIWorkout spec

--- spec/factories/lti_workouts.rb
+++ spec/factories/lti_workouts.rb
@@ -1,9 +1,9 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
-FactoryGirl.define do
+FactoryBot.define do
   factory :lti_workout do
-    workout nil
-    lms_instance nil
-    lms_assignment_id "MyString"
+    workout { nil }
+    lms_instance { nil }
+    lms_assignment_id { "MyString" }
   end
 end
