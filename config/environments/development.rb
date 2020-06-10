eb0e28a7f76cac484774a0dfbdd67a0d650136ac
Ayaan Kazerouni <ayaan@vt.edu>
2020-06-09 23:47:52 -0400

    Docker-compose for databases

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  Rails.env = 'development'
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
