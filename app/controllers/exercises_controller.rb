d1ede2dd93d56451218026da73f5c0b11c32450a
Ayaan Kazerouni <ayaan@vt.edu>
2020-07-09 16:42:10 -0400

    Fixes #160
    Better error messages + links to exercises after creating or
    editing

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -342,33 +342,35 @@ class ExercisesController < ApplicationController
 
     # parse the text_representation
     exercises = ExerciseRepresenter.for_collection.new([]).from_hash(hash)
+    success_all = true
+    error_msgs = []
+    success_msgs = []
     exercises.each do |e|
       if !e.save
+        success_all = false
         # put together an error message
-        errors = []
-        errors <<  "Cannot save exercise:<ul>"
+        error_msgs <<  "Errors while saving exercise #{e.andand.name}:<ul>"
         e.errors.full_messages.each do |msg|
-          errors << "<li>#{msg}</li>"
+          error_msgs << "<li>#{msg}</li>"
         end
-
-        if e.current_version
-          e.current_version.errors.full_messages.each do |msg|
-            errors << "<li>#{msg}</li>"
-          end
-          if e.current_version.prompts.any?
-            e.current_version.prompts.first.errors.full_messages.each do |msg|
-              errors << "<li>#{msg}</li>"
-            end
-          end
-        end
-        errors << "</ul>"
-        redirect_to @return_to, flash: { error: errors.join("").html_safe } and return
+        error_msgs << "</ul>"
       else # successfully created the exercise
         exercise_collection.andand.add(e, override: true)
+        success_msgs <<
+          "<li>X#{e.id}: #{e.name} saved, try it #{view_context.link_to 'here', exercise_practice_path(e)}.</li>"
       end
     end
 
-    redirect_to @return_to, flash: { success: 'Exercise saved!' }
+    if success_all
+      success_msgs = 'Success!<ul>' + success_msgs.join("") + "</ul>"
+      redirect_to @return_to, flash: { success: success_msgs.html_safe } and return
+    else
+      if !success_msgs.blank?
+        error_msgs << "Some exercises were successfully saved."
+        error_msgs << "<ul>" + success_msgs.join("") + "</ul>"
+      end
+      redirect_to @return_to, flash: { error: error_msgs.join("").html_safe } and return
+    end
   end
 
 	def embed
