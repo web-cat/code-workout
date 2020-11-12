9b71517089eb6dacca751bbbe097fde69f09a7bd
Alex Hicks <awh4kc@virginia.edu>
2020-11-12 11:22:26 -0500

    removed ssl
    Signed-off-by: Alex Hicks <awh4kc@virginia.edu>

--- runservers.sh
+++ runservers.sh
@@ -1,1 +1,1 @@
-RUBYOPT='-W:no-deprecated' RAILS_ENV=development bundle exec thin start --ssl --ssl-key-file server.key --ssl-cert-file server.crt -p 9292 --debug
+RUBYOPT='-W:no-deprecated' RAILS_ENV=development bundle exec thin start -p 9292 --debug
