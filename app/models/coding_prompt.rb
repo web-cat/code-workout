98bcb7a2105d0b1929e418cc5696f4f72dd62af2
s-edwards <edwards@cs.vt.edu>
2021-11-11 12:05:01 -0500

    support for raw CxxTest classes in C++ questions

--- app/models/coding_prompt.rb
+++ app/models/coding_prompt.rb
@@ -119,6 +119,10 @@ class CodingPrompt < ActiveRecord::Base
         if self.test_script =~ /\s*(import[^,;"']*;|public\s+class)/
           raise NotImplementedError, "regenerate_JUnit_tests not implemented yet!"
         end
+      when 'C++'
+        if self.test_script =~ /\s*(using\s+namespace|#(define|undef|ifdef|ifndef|include|if|line)\s)/
+          raise NotImplementedError, "regenerate_JUnit_tests not implemented yet!"
+        end
       end
       # Default, if none of above cases return
       generate_CSV_tests(test_file_name)
@@ -168,6 +172,11 @@ class CodingPrompt < ActiveRecord::Base
       when 'Java'
         if self.test_script =~ /\s*(import[^,;"']*;|public\s+class)/
           parse_JUnit_tests
+          return
+        end
+      when 'C++'
+        if self.test_script =~ /\s*(using\s+namespace|#(define|undef|ifdef|ifndef|include|if|line)\s)/
+          parse_CxxTest_tests
           return
         end
       end
@@ -259,6 +268,130 @@ class CodingPrompt < ActiveRecord::Base
     junit.gsub!(
       /((?:\p{Blank}*\/\/.*\n)|(?:\p{Blank}*\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/\p{Blank}*\n?))*((?:\s*@(?:Test|Example|Hidden|Screening|(?:Weight|ScoringWeight)\s*\(\s*[0-9]+(?:\.[0-9]*)?\s*\)|(?:Description|Hint|NegativeFeedback)\s*\(\s*"\s*(?:[^"]|\\")*\s*"\s*\)))*)(\s*public\s+void\s+)([a-zA-Z0-9_]+)(\s*\(\s*\))/
       ) do |match|
+      comment = Regexp.last_match(1)
+      attrs = Regexp.last_match(2)
+      publicvoid = Regexp.last_match(3)
+      name = Regexp.last_match(4)
+      args = Regexp.last_match(5)
+
+      if name =~ /^test/ || attrs =~ /@Test\b/
+        tc = TestCase.new(
+          weight: 1.0,
+          coding_prompt: self,
+          input: '',
+          expected_output: '',
+          example: false,
+          hidden: false,
+          static: false,
+          screening: false)
+
+        tc.description = '<No Test Description Provided!>'
+        desc = nil
+        # Attempt to pull description string from comments
+        if comment =~ /description\s*:\s*((?:[^*\r\n]|(?:\*+[^*\/\r\n]))*)(?:\*\/\s*)?$/i
+          desc = $1
+        end
+        # Attempt to pull description string from attribute, which overrides
+        if attrs =~ /@(?:Description|Hint)\s*\(\s*"\s*((?:[^"]|\\")*)\s*"\s*\)/
+          desc = $1.gsub(/\\"/, '"')
+        end
+        # If no description, try to pull it from the method name
+        if desc.blank? && name =~ /^(?:test)?(.*)(?:_*[0-9]+)?$/
+          namedesc = $1
+          if !namedesc.blank?
+            namedesc = namedesc.sub(/^_+/, '').sub(/_+$/, '')
+            if !namedesc.blank?
+              if namedesc =~ /^((?:(?:example|screening|hidden)_)+)([^_].*)$/
+                prefix = $1
+                suffix = $2
+              else
+                prefix = ''
+                suffix = namedesc
+              end
+              if suffix.blank?
+                if !prefix.blank?
+                  desc = prefix
+                end
+              else
+                desc = prefix.gsub(/_/, ':') + suffix.underscore.split(/_+/).join(' ').capitalize
+              end
+            end
+          end
+        end
+        tc.parse_description_specifier(desc)
+
+        # look for "example" tag in comments or attribute
+        if comment =~ /example\s*:\s*true\s*(?:\*\/\s*)?$/i || attrs =~ /@Example\b/
+          tc.example = true
+        end
+        # look for "hidden" tag in comments or attribute
+        if comment =~ /hidden\s*:\s*true\s*(?:\*\/\s*)?$/i || attrs =~ /@Hidden\b/
+          tc.hidden = true
+        end
+        # look for "screening" tag in comments or attribute
+        if comment =~ /screening\s*:\s*true\s*(?:\*\/\s*)?$/i || attrs =~ /@Screening\b/
+          tc.screening = true
+        end
+
+        # Attempt to pull negative feedback string from comments
+        nfb = nil
+        if comment =~ /negative\s*feedback\s*:\s*((?:[^*\r\n]|(?:\*+[^*\/\r\n]))*)(?:\*\/\s*)?$/i
+          nfb = $1
+        end
+        # Attempt to pull negative feedback string from attributes
+        if attrs =~ /@NegativeFeedback\s*\(\s*"\s*((?:[^"]|\\")*)\s*"\s*\)/
+          nfb = $1.gsub(/\\"/, '"')
+        end
+        tc.parse_negative_feedback_specifier(nfb)
+
+        # Attempt to pull test case weight from comments
+        if comment =~ /(?:scoring\s*)?weight\s*:\s*([0-9]+(?:\.[0-9]*)?)\s*(?:\*\/\s*)?$/i
+          tc.weight = $1.to_f
+        end
+        # Attempt to pull test case weight from attributes
+        if attrs =~ /@(?:Scoring)?Weight\s*\(\s*([0-9]+(?:\.[0-9]*)?)\s*\)/
+          tc.weight = $1.to_f
+        end
+
+        if tc.save
+          self.test_cases << tc
+          # Rename test method to include TestCase id
+          name = "#{name}_#{tc.id}"
+        else
+          puts "error saving test case: #{tc.errors.full_messages.to_s}"
+        end
+      end
+
+      rewrite = "#{comment}#{attrs}#{publicvoid}#{name}#{args}"
+      # puts "rewritten test decl:\n#{rewrite}\n\n"
+      rewrite
+    end
+    # puts "junit after rewrite:\n#{junit}"
+    File.write(test_file_name, junit)
+  end
+
+
+  # -------------------------------------------------------------
+  def parse_CxxTest_tests
+    junit = self.test_script.gsub(/\r\n/, "\n")
+
+    # First, collect any embedded static tests
+    junit.scan(
+      /(?:\/\/\p{Blank}*static\p{Blank}*tests\p{Blank}*:\p{Blank}*(.*\n(?:\p{Blank}*\/\/.*\n)*))|(?:\/\*\s*static\s*tests\s*:\s*((?:[^*]|(?:\*+[^*\/]))*)\*+\/)/i
+    ) do |tests1, tests2|
+      if tests2.blank?
+        tests = tests1.gsub(/^\p{Blank}*\/\/\p{Blank}*/, '').gsub(/\p{Blank}*$/, '')
+      else
+        tests = tests2.gsub(/^\p{Blank}*(\*\p{Blank}*)?/, '').gsub(/\p{Blank}*$/, '')
+      end
+      tests.sub!(/\n*$/m, "\n")
+      parse_CSV_tests(tests)
+    end
+
+    # Now, extract metadata about and rename each test method
+    junit.gsub!(
+      /((?:\p{Blank}*\/\/.*\n)|(?:\p{Blank}*\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/\p{Blank}*\n?))*((?:\s*@(?:Test|Example|Hidden|Screening|(?:Weight|ScoringWeight)\s*\(\s*[0-9]+(?:\.[0-9]*)?\s*\)|(?:Description|Hint|NegativeFeedback)\s*\(\s*"\s*(?:[^"]|\\")*\s*"\s*\)))*)(\s*void\s+)([a-zA-Z0-9_]+)(\s*\(\s*\))/
+    ) do |match|
       comment = Regexp.last_match(1)
       attrs = Regexp.last_match(2)
       publicvoid = Regexp.last_match(3)
