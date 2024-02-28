d2f4901fea266d0ed65b1186ba988d651e0aa0cb
“zkewei19” <zkewei19@vt.edu>
2024-02-28 16:27:53 -0500

    Add files to make it work for different Jsparsons problem types

---
+++ app/assets/javascripts/simple_code.js
@@ -0,0 +1,78 @@
+var parson;
+$(document).ready(function(){ 
+    var index = window.location.pathname.split('/').pop().split('.')[0];
+    var score = 0;
+    var data;
+    Sk.canvas = "studentCanvas";
+    $.getJSON('/data/simple_code.json', function(response) {
+        data = response;
+        var initial = data[index]['initial'];
+        var initialArray = initial.split('\n');
+        var config = data[index]['parsonsConfig'];
+        document.getElementById("title").innerHTML = data[index].title;
+        document.getElementById("instructions").innerHTML = data[index].instructions;
+        config.sortableId = 'sortable';
+        config.trashId = 'sortableTrash';
+        console.log(data[index]['parsonsConfig']['turtleModelCode']);
+        // 如果config中有turtleModelCode，就把grader改成TurtleGrader
+        if (data[index]['parsonsConfig']['turtleModelCode']) {
+            config.grader = ParsonsWidget._graders.TurtleGrader;
+            console.log("有乌龟")
+            
+        } else {
+            config.grader = ParsonsWidget._graders.LanguageTranslationGrader;
+        }
+        parson = new ParsonsWidget(config);
+        parson.init(initialArray);
+        parson.shuffleLines();
+    });
+    $("#newInstanceLink").click(function(event){
+        event.preventDefault();
+        parson.shuffleLines();
+    });
+    $("#feedbackLink").click(function(event){
+        event.preventDefault();
+        var fb = parson.getFeedback();
+        if (data[index]['parsonsConfig']['turtleModelCode']){
+            if (fb.success) {
+                //把$("#feedback").html(fb.feedback)改成“Great job, you solved the exercise!”
+                $("#feedback").html("Great job, you solved the exercise!");
+            } else {
+                $("#feedback").html("Sorry, your solution does not match the model image");
+            }
+        }else{
+            $("#feedback").html(fb.feedback);
+        }
+        // $("#unittest").html("<h2>Feedback from testing your program:</h2>" + fb.feedback);
+        if (fb.success) {
+            score = 50;
+        } else {
+            score = 0;
+        }
+        updateScore(score);
+    });
+    // function for updating the score
+    function updateScore(score) {
+        //get external_id from the data attribute
+        var externalId = $('#exercise-data').data('external-id');
+        
+        $.ajax({
+            url: '/update_score',
+            type: 'POST',
+            data: JSON.stringify({ 
+                experience: score,
+                external_id: externalId  
+            }),
+            contentType: 'application/json',
+            headers: {
+                'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')  // CSRF token
+            },
+            success: function(response) {
+                console.log('Score updated successfully');
+            },
+            error: function(xhr) {
+                console.log('Failed to update score: ' + xhr.responseText);
+            }
+        });
+    }
+});
---
+++ app/assets/javascripts/simple_pseudocode.js
@@ -0,0 +1,61 @@
+var initial = 'IF $$toggle::a::b$$ $$toggle::<::>::<>$$ b THEN\n  min := a\nELSE\n  min := b\nENDIF';
+var parson;
+$(document).ready(function(){
+    var score = 0;
+    parson = new ParsonsWidget({
+        'sortableId': 'sortable',
+        'trashId': 'sortableTrash',
+        'max_wrong_lines': 1,
+        'vartests': [{initcode: "min = None\na = 0\nb = 2", code: "", message: "Testing with a = 0 ja b = 2", variables: {min: 0}},
+            {initcode: "min = None\na = 7\nb = 4\n", code: "", message: "Testing with a = 7 ja b = 4",
+              variables: {min: 4}}],
+        'grader': ParsonsWidget._graders.LanguageTranslationGrader,
+        'executable_code': "if $$toggle$$ $$toggle::<::>::!=$$ b:\n" +
+              "min = a\n" +
+              "else:\n" +
+              "min = b\n  pass",
+        'programmingLang': "pseudo"
+    });
+    var initialArray = initial.split('\n');
+    parson.init(initialArray);
+    parson.shuffleLines();
+    $("#newInstanceLink").click(function(event){
+        event.preventDefault();
+        parson.shuffleLines();
+    });
+    $("#feedbackLink").click(function(event){
+        event.preventDefault();
+        console.log($('#sortablecodeline0').html());
+        var fb = parson.getFeedback();
+        $("#feedback").html(fb.feedback);
+        // $("#unittest").html("<h2>Feedback from testing your program:</h2>" + fb.feedback);
+        if (fb.success) {
+            score = 50;
+            updateScore(score);
+        }
+    });
+    // function for updating the score
+    function updateScore(score) {
+        //get external_id from the data attribute
+        var externalId = $('#exercise-data').data('external-id');
+        
+        $.ajax({
+            url: '/update_score',
+            type: 'POST',
+            data: JSON.stringify({ 
+                experience: score,
+                external_id: externalId  
+            }),
+            contentType: 'application/json',
+            headers: {
+                'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')  // CSRF token
+            },
+            success: function(response) {
+                console.log('Score updated successfully');
+            },
+            error: function(xhr) {
+                console.log('Failed to update score: ' + xhr.responseText);
+            }
+        });
+    }
+});
